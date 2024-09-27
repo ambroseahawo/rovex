8d95d6e44db936700f248b2fe010c6a7af44e585
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-09-28 00:55:27 +0300

    Implement rover module

--- lib/rovex.ex
+++ lib/rovex.ex
@@ -1,18 +1,24 @@
-defmodule Rovex do
+defmodule Rover do
   @moduledoc """
   Documentation for `Rovex`.
   """
+  use GenServer
 
-  @doc """
-  Hello world.
+  defstruct [:x, :y, :direction, :name]
 
-  ## Examples
+  def init({x, y, d, name}) do
+    {:ok, %Rover{x: x, y: y, direction: d, name: name}}
+  end
 
-      iex> Rovex.hello()
-      :world
+  def get_state(name) do
+    GenServer.call(String.to_atom(name), :get_state)
+  end
 
-  """
-  def hello do
-    :world
+  def start_link({x, y, d, name}) do
+    GenServer.start_link(__MODULE__, {x, y, d, name}, name: String.to_atom(name))
+  end
+
+  def handle_call(:get_state, _from, state) do
+    {:reply, {:ok, {state.x, state.y, state.direction, state.name}}, state}
   end
 end
